32828b9b64d27d5bc6c431e2f8d50e3c3e7f7747
Suin Kim <contact@suinkim.me>
2025-05-17 03:13:40 +0900

    크롬 확장팩 테스트 코드 작성

---
+++ src/content.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.body).render(<h1>Hello Jungle!</h1>);
+
+console.log('Hello Jungle');
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,20 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        popup: path.join(__dirname, 'index.html'),
+        content: path.join(__dirname, 'src/content.tsx'),
+        background: path.join(__dirname, 'src/background.ts'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
